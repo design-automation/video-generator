f260423dda3b0b826b7764d87f323e506d80a97b
pkered <derekpung@hotmail.com>
2019-12-23 15:22:15 +0800

    [fix] apostrophe character causing trouble for edx generator

--- _xml_friendly.py
+++ _xml_friendly.py
@@ -1,5 +1,5 @@
 def to_xml(str_to_xml):
-    return str_to_xml.replace("&","&amp;").replace("<", "&lt;").replace(">", "&gt;")
+    return str_to_xml.replace("&","&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("'", "&apos;")
 
 def to_symbol(str_to_symbol):
-    return str_to_symbol.replace("&amp;","&").replace("&lt;", "<").replace("&gt;", ">")
+    return str_to_symbol.replace("&amp;","&").replace("&lt;", "<").replace("&gt;", ">").replace("&apos;", "'")
