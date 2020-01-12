2727eac7783c51a8b08c7b7a05d38b615ce37dc3
pkered <derekpung@hotmail.com>
2020-01-12 16:18:18 +0800

    [fix] ignore files which start with ~$

--- _get_by_type.py
+++ _get_by_type.py
@@ -1,9 +1,9 @@
 import glob
-import re
+import os
 
 def get_paths_by_typ(fdr_path,typ):
     paths = glob.glob(fdr_path + "\\*.%s" % (typ))
+    ret_paths = [path for path in paths if not os.path.basename(path).startswith("~$")]
     # if (len(paths)==0):
     #     raise Exception ("No .%s file found in \\%s." % (typ, fdr_path))    
-    return paths
-    
+    return ret_paths
